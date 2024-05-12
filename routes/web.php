320a9e61578aee63bc27ef90a39fcf779476ccf8
StebanG4 <egarcia93@unab.edu.co>
2024-05-11 23:08:31 -0500

    creacion de proyecto agregango las migraciones y controladores

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'role:user'])->name('dashboard');
+
+Route::middleware(['auth', 'role:user'])->group(function(){
+    Route::controller(DashboardController::class)->group(function(){
+        Route::get('/admin/dashboard','Index');
+    });
+});
+
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
